6a99e49c416d1297d089301989e1e117b0870449
ppnagansure <prashant.nagansure@iiitb.org>
2014-12-14 16:03:26 +0530

    changes in classify.py

--- How_Humans_Sketch_CS706/classify.py
+++ How_Humans_Sketch_CS706/classify.py
@@ -4,10 +4,14 @@ from cPickle import load
 from learn import extractSift, computeHistograms, writeHistogramsToFile
 import os
 
+#default file paths
 HISTOGRAMS_FILE = 'testdata.svm'
 CODEBOOK_FILE = 'codebook.file'
 MODEL_FILE = 'trainingdata.svm.model'
 
+
+#parsing for classification purpose 
+#python classify.py -c path_to_folders_with_images/codebook.file -m path_to_folders_with_images/trainingdata.svm.model images_you_want_to_classify
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='classify images with a visual bag of words model')
@@ -15,15 +19,21 @@ def parse_arguments():
     parser.add_argument('-m', help='path to the model  file', required=False, default=MODEL_FILE)
     parser.add_argument('input_images', help='images to classify', nargs='+')
     args = parser.parse_args()
+	#print('check for arguments passed :')
+	#print('path to the model  file',args.m)
+	#print('path to the codebook file',args.c)
+	#print(args.input_images)
     return args
 
+	
+#arguments are passed for predicting image	
 def predict_image(model_file,codebook_file,fnames):
 	print str(fnames[0])+".sift"
 	try:
 		os.remove(str(fnames[0])+".sift")
 	except OSError:
 		pass
-	# extract Sift features"
+	# extract Sift features for each individual image"
 	all_files = []
 	all_files_labels = {}
 	all_features = {}
@@ -35,6 +45,7 @@ def predict_image(model_file,codebook_file,fnames):
 			all_files_labels[i] = 0  # label is unknown
 			
 		# loading codebook from codebook_file
+		# default codebookfile: codebook.file
 		with open(codebook_file, 'rb') as f:
 			codebook = load(f)
 
@@ -46,8 +57,7 @@ def predict_image(model_file,codebook_file,fnames):
 
 		# write the histograms to file to pass it to the svm
 		nclusters = codebook.shape[0]
-		writeHistogramsToFile(nclusters,
-							  all_files_labels,
+		writeHistogramsToFile(nclusters, all_files_labels,
 							  fnames,
 							  all_word_histgrams,
 							  HISTOGRAMS_FILE)
